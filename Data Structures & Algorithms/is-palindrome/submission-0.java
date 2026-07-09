3700b1e4349d79d412808adfe81bd415e3e3ad5d
Gauri <109908146+GauriMandlik@users.noreply.github.com>
2026-07-09 11:20:11 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+     s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+
+     String s1= new StringBuilder(s).reverse().toString();
+
+     return s.equals(s1);
+    }
+}
